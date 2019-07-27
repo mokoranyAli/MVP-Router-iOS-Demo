99fa70b7faff8e3766f7f5de72285d55559fe153
Mohamed Korany Ali <mohammed.korany.ali@gmail.com>
2019-07-27 02:17:48 +0200

    first commit

---
+++ MVP-Router-Demo/Scenes/User Details Scenario/View Controller/UserDetailsVC+Delegate.swift
@@ -0,0 +1,9 @@
+//
+//  UserDetailsVC+Delegate.swift
+//  MVP-Router-Demo
+//
+//  Created by Mohamed Korany Ali on 7/26/19.
+//  Copyright © 2019 Mohamed Korany Ali. All rights reserved.
+//
+
+import Foundation
